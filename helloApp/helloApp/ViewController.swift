30ca1e52c33642b5e8bfd2e8a183c1c081999dff
ShovelSquid <kaelen1cook@gmail.com>
2019-09-18 10:43:40 -0700

    Made it work
    Added switch, good and bad options and lists and all around nifty staff

--- helloApp/helloApp/ViewController.swift
+++ helloApp/helloApp/ViewController.swift
@@ -9,18 +9,45 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet weak var nameTextField: UITextField!
     @IBOutlet weak var greetingLabel: UILabel!
+    @IBOutlet weak var goodnBadJobSwitch: UISwitch!
+    @IBOutlet weak var helloButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        nameTextField.text = "Riley"
-        sayHello("")
+        helloButton.setTitle("Good Job", for: .normal)
+        nameTextField.text = "Jesus"
+        greetingLabel.text = ""
     }
-
+    var badJobList = ["You screwed it up", "Come on", "You can do better", "Horrible job", "You suck"]
+    var goodJobList = ["Good job", "Nicely done", "OORAH", "Mission accomplished"]
+    
     @IBAction func sayHello(_ sender: Any) {
-        greetingLabel.text = "Hello, \(nameTextField.text ?? "human")!"
+        var Job: String
+        if goodnBadJobSwitch.isOn {
+            Job = goodJobList.randomElement()!
+        }
+        else {
+            Job = badJobList.randomElement()!
+        }
+        if nameTextField.text != nil && nameTextField.text == "Billy" {
+            greetingLabel.text = "Die, Billy"
+        }
+        else {
+            greetingLabel.text = "\(Job), \(nameTextField.text ?? "human")."
+        }
+    }
+    
+    @IBAction func toggleSwitch(_ sender: UISwitch) {
+        let yes = sender.isOn
+        if yes {
+            helloButton.setTitle("Good Job", for: .normal)
+        }
+        else {
+            helloButton.setTitle("Bad Job", for: .normal)
+        }
     }
     
 }
