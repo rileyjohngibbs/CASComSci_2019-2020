8e7d925deb21fd0d1b01c2da0d023e21f4ec5c8e
danilubezki <48220191+danilubezki@users.noreply.github.com>
2019-10-02 09:03:56 -0700

    Add files via upload

---
+++ Instagramy/Instagramy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Instagramy
+//
+//  Created by Dani Lubezki on 10/2/19.
+//  Copyright © 2019 Dani Lubezki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
