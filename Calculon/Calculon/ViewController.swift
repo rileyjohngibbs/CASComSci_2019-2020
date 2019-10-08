042c99b629dc75e3a418b9217598010eb03088bb
ShovelSquid <kaelen1cook@gmail.com>
2019-10-08 09:58:06 -0700

    styling

---
+++ Calculon/Calculon/ViewController.swift
@@ -0,0 +1,115 @@
+//
+//  ViewController.swift
+//  Calculon
+//
+//  Created by Kaelen Cook on 10/7/19.
+//  Copyright © 2019 Kaelen Cook. All rights reserved.
+//
+
+import UIKit
+enum modes {
+    case not_set
+    case addition
+    case subtraction
+    case multiplication
+}
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var Label: UILabel!
+    var labelString: String = "0"
+    var currentMode: modes = .not_set
+    var savedNum: Int = 0
+    var lastButtonWasMode: Bool = false
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func didPressEquals(_ sender: Any) {
+        guard let labelInt: Int = Int(labelString) else {
+            return
+        }
+        
+        if (currentMode == .not_set || lastButtonWasMode) {
+            return
+        }
+        
+        if (currentMode == .addition) {
+            savedNum += labelInt
+        }
+        
+        if (currentMode == .subtraction) {
+            savedNum -= labelInt
+        }
+        
+        if (currentMode == .multiplication) {
+            savedNum *= labelInt
+        }
+        
+        currentMode = .not_set
+        labelString = "\(savedNum)"
+        updateText()
+        lastButtonWasMode = true
+    }
+    
+    @IBAction func didPressPlus(_ sender: Any) {
+        changeMode(newMode: .addition)
+    }
+    
+    @IBAction func didPressMinus(_ sender: Any) {
+        changeMode(newMode: .subtraction)
+    }
+    
+    @IBAction func didPressMultiply(_ sender: Any) {
+        changeMode(newMode: .multiplication)
+    }
+    
+    @IBAction func didPressClear(_ sender: Any) {
+        labelString = "0"
+        currentMode = .not_set
+        lastButtonWasMode = false
+        savedNum = 0
+        Label.text = "0"
+    }
+    
+    
+    @IBAction func pressingNumber(_ sender: UIButton) {
+        sender.backgroundColor = UIColor.black
+    }
+    
+    @IBAction func pressDoneNumber(_ sender: UIButton) {
+        sender.backgroundColor = UIColor.darkGray
+    }
+    
+    
+    @IBAction func didPressNumber(_ sender: UIButton) {
+        let stringValue:String? = sender.titleLabel?.text
+        
+        labelString = labelString.appending(stringValue!)
+        updateText()
+    }
+    
+    func updateText(){
+        guard let labelInt: Int = Int(labelString) else {
+            return
+        }
+        if (currentMode == .not_set) {
+            savedNum = labelInt
+        }
+        Label.text = "\(labelInt)"
+    }
+    
+    func changeMode(newMode: modes) {
+        if (savedNum == 0) {
+            return
+        }
+        
+        currentMode = newMode
+        lastButtonWasMode = true
+        
+    }
+    
+}
+
