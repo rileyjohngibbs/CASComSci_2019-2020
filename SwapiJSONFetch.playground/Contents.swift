cb182921a3f1a1c1000f082bdb9dc065a397b0d5
Riley John Gibbs <rileyjohngibbs@gmail.com>
2019-12-04 21:05:44 -0800

    Update SwapiJSONFetch to demo sync vs. async

--- SwapiJSONFetch.playground/Contents.swift
+++ SwapiJSONFetch.playground/Contents.swift
@@ -1,25 +1,51 @@
 import UIKit
 
 func main() {
-    let swapiUrlStr = "https://swapi.co/api/people/3/"
-    let swapiUrlObj = URL(string: swapiUrlStr)!
+    let MAX_ID: Int = 15
+    let swapiUrlStr = "https://swapi.co/api/people/"
     
-    let task = URLSession.shared.dataTask(with: swapiUrlObj) {
-        (data, response, error) in
-        guard let httpResponse = response as? HTTPURLResponse,
-            (200...299).contains(httpResponse.statusCode),
-            let data = data else {
-            print("URL fetch failed")
-            return
+    print("Fetching the first \(MAX_ID) characters synchronously. This is slow.")
+    var syncPeople: [SwapiPerson] = []
+    for i: Int in (1...MAX_ID) {
+        let swapiUrlObj = URL(string: "\(swapiUrlStr)\(i)/")!
+        let data = try? Data(contentsOf: swapiUrlObj)
+        if let data = data {
+            if let person = parseSwapiPerson(data) {
+                syncPeople.append(person)
+            }
         }
-        guard let person = parseSwapiPerson(data) else {
-            print("JSON parsing to SWAPI person data failed")
-            return
-        }
-        print(person)
     }
+    syncPeople.sort(by: {(a: SwapiPerson, b: SwapiPerson) in a.name < b.name})
+    for person in syncPeople { print(person) }
     
-    task.resume()
+    print("Fetching the first \(MAX_ID) character asynchronously. This should be pretty quick.")
+    var tasks: [URLSessionDataTask] = []
+    var asyncPeople: [SwapiPerson] = []
+    for i: Int in (1...MAX_ID) {
+        let swapiUrlObj = URL(string: "\(swapiUrlStr)\(i)/")!
+        let task = URLSession.shared.dataTask(with: swapiUrlObj) {
+            (data, response, error) in
+            guard let httpResponse = response as? HTTPURLResponse,
+                (200...299).contains(httpResponse.statusCode),
+                let data = data else {
+                print("URL fetch failed")
+                return
+            }
+            guard let person = parseSwapiPerson(data) else {
+                print("JSON parsing to SWAPI person data failed")
+                return
+            }
+            asyncPeople.append(person)
+            if asyncPeople.count == MAX_ID {
+                asyncPeople.sort(by: {(a: SwapiPerson, b: SwapiPerson) in a.name < b.name})
+                for person in asyncPeople { print(person) }
+            }
+        }
+        tasks.append(task)
+    }
+    for task in tasks {
+        task.resume()
+    }
 }
 
 struct SwapiPerson {
