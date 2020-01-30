d397bf47802122ef1f8cb4ec6cb6bbf58abc7f65
danilubezki <48220191+danilubezki@users.noreply.github.com>
2020-01-29 20:14:33 -0800

    Add files via upload

---
+++ layoutPracticeExcersise/layoutPracticeExcersise/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  layoutPracticeExcersise
+//
+//  Created by Dani Lubezki on 1/29/20.
+//  Copyright © 2020 Dani Lubezki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
